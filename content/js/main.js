57ad661048b5fdb3c3512e4044ff7de04239a094
afketo <afketo@gmail.com>
2022-05-12 18:20:17 +0200

    Update main.js

--- content/js/main.js
+++ content/js/main.js
@@ -4,7 +4,7 @@
 	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
 
-$(document).ready(function() {
+$(document).ready(functiona() {
 
   /**
    * Author: Heather Corey
